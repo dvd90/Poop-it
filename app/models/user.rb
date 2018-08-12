267aa0afb0f6bc5616694508f0b5150a936491d7
David Sellam <dvdsellam@gmail.com>
2018-08-12 17:36:20 +0300

    controller mostly done add some func inside models user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  def renter?
+    false
+  end
+  def owner?
+    false
+  end
 end
