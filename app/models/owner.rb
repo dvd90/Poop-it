91d690bd4f4c8eaf4156547d899aa09d8739c753
Lorene Valency <lorene.herzog@gmail.com>
2018-08-12 17:54:28 +0300

    validations done

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -1,4 +1,5 @@
 class Owner < User
   has_many :bookings
   has_many :toilets, through: :bookings
+  validate :picture, presence: true
 end
--- app/models/toilet.rb
+++ app/models/toilet.rb
@@ -1,4 +1,8 @@
 class Toilet < ApplicationRecord
   belongs_to :owner
   has_many :bookings
+  validates :title, presence: true, uniqueness: true
+  validates :description, presence: true
+  validates :location, presence: true
+  validates :price, presence: true
 end
