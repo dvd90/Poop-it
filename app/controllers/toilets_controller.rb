9168cdc7fba4c8b027713b6d47b97266de7a2b1c
David Sellam <dvdsellam@gmail.com>
2018-08-13 21:31:06 +0300

    Funny Changes and new views for user and authorizations

--- app/controllers/toilets_controller.rb
+++ app/controllers/toilets_controller.rb
@@ -1,4 +1,5 @@
 class ToiletsController < ApplicationController
+    skip_before_action :authenticate_user!, only: :index
     before_action :set_toilet, only: [:show, :edit, :update, :destroy]
   def index         # GET
     @toilets = policy_scope(Toilet)
